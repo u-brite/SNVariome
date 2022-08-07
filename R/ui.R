ce4a2232cb5c67535318d441c63f6679d4d2793b
Hailey <86804021+chaoticsci@users.noreply.github.com>
2022-08-06 19:43:47 -0500

    Update ui.R

---
+++ R/ui.R
@@ -0,0 +1,19 @@
+shinyUI(
+  navbarPage(inverse = TRUE,
+             title = "SNVariome",
+             theme = shinytheme("journal"),
+             tabPanel("MyGene Tool",
+                      tags$footer(verbatimTextOutput("citemygene"),
+                      sidebarLayout(
+                        sidebarPanel(textInput("accession", "Input Gene or Variant", value="NM_000680.3"),
+                                     width = 4),
+                        mainPanel(dataTableOutput('mygene_table'),
+                                  width = 13)
+                      )
+             )),
+             navbarMenu("About",
+                        tabPanel("Links"),
+                        tabPanel("Team",
+                                 fluidRow(includeMarkdown("about.md")
+                                   ),))
+             ))
