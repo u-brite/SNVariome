f03d65150ddbac7a7243edfb4c93eb97fc2cec6b
sdhutchins <sdhutchins@uab.edu>
2022-08-22 18:04:40 -0500

    Tinker with R api access.

--- app/api.R
+++ app/api.R
@@ -3,11 +3,18 @@ library(jsonlite)
 library(xml2)
 
 # API End point from Sigcom LINCS
-server <- "https://maayanlab.cloud/sigcom-lincs/metadata-api/resources/count"
+server2 <- "https://maayanlab.cloud/sigcom-lincs/metadata-api/signatures/{HTR1A}"
+server <- "https://ldp3.cloud/metadata-api/libraries/find"
+
+payload <- '{"filter": {
+  "where": {
+    "meta": {
+        "fullTextSearch": "proteomics"}},
+    "limit": 2}}'
   
-r <- GET(paste(server), content_type("application/json"))
+r <- POST(paste(server), body = payload, encode = "json", content_type("application/json"))
 
 stop_for_status(r)
 
-head(data.frame(t(sapply(content(r),c))))
+#head(data.frame(t(sapply(content(r),c))))
 head(fromJSON(toJSON(content(r))))
