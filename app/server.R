b439530d4b718d7d3ca01aa9c40909c7396b028f
sdhutchins <sdhutchins@uab.edu>
2022-08-22 18:05:04 -0500

    Implemented myvariant.

--- app/server.R
+++ app/server.R
@@ -1,17 +1,17 @@
 library(shiny)
-library(mygene)
 library(DT)
 library(shinyjs)
 library(shinythemes)
+library(myvariant)
 
 shinyServer(function(input, output) {
   
-  mygene_results <- reactive({
-    queryMany(input$accession, scopes="accession", fields=c("entrezgene", "symbol", "name", "summary"), species="human")
+  myvar_results <- reactive({
+    queryVariants(input$variant, scopes=c("clinvar.gene.symbol", "clinvar.rcv.accession", "clinvar.rsid", "dbnsfp.clinvar.hgvs"), fields="clinvar.gene.symbol, clinvar.rsid, clinvar.type, dbnsfp.clinvar.trait, dbnsfp.clinvar.review")
   })
   
   output$citeus <- renderText({ invisible("Shaurita Hutchins, Bernhard Hane, Oladosu Tosin Ayodeji, Bharat Mishara, Hailey Jori Levi, Pooja Singaravelu (2022).") })
   
   
-  output$mygene_table <- renderDataTable({as.data.frame(mygene_results())})
+  output$myvar_table <- renderDataTable({subset(as.data.frame(myvar_results()), select = -c(2,3,4,8))}, width = "100%", height = "auto")
 })
